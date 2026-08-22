c8601d755b5350531163f2706f794d3ce1646465
Mohit <mohitbaghel1234321@gmail.com>
2026-08-22 17:26:02 +0530

    fdlk

---
+++ Assisment2.py
@@ -0,0 +1,13 @@
+a = int(input("enter first number : "))
+b = int(input("enter Second number : "))
+
+
+sum = (a+b)
+Diff = (a-b)
+pro = (a*b)
+Quo = (a/b)
+
+print("Add is :",sum)
+print("Sub",Diff)
+print("Mul",pro)
+print("Div",Quo)
---
+++ Assisment3.py
@@ -0,0 +1,9 @@
+
+
+a = int(input("enter the first value :"))
+b = int(input("enter the Second value :"))
+c = float(input("enter the third value :"))
+
+Avg =( a+b+c)/3
+
+print(Avg)
