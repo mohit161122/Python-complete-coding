cfed84fbbde0509afc10c0fa4fbf6b7d603c4cab
Mohit <mohitbaghel1234321@gmail.com>
2026-08-23 11:50:58 +0530

    ds

--- Day2 in Python/Conditional.py
+++ Day2 in Python/Conditional.py
@@ -13,3 +13,68 @@
 #     print("you can't vote ")
    
 
+# color = input("Write a color :")
+
+# if  (color == "Red"):
+#     print("Stop")
+# elif (color == "Green"):
+#     print("Go")
+# elif (color == "Yellow"): 
+#     print("Look")
+# else:
+#     print("Wrong color For traffic light")
+
+
+
+
+# age = int(input("Enter your Age :"))
+
+# if (age < 13 ):
+#     print("Child")
+# elif(age >= 13) and (age < 18):
+#     print("Teeneger")
+# else:
+#     print("Adult")
+
+
+
+
+
+
+# username = input("Enter the username : ")
+# password = input("Enter the password : ")
+
+# if (username == "admin") and (password == "pass"):
+#     print("Login Sucessfully")
+# elif(username != "admin"):
+#     print("Wrong Username")
+# elif(password != "pass"):
+#     print("wrong password")
+# else:
+#     print("wrong entry ")
+
+
+
+
+
+
+# number = int(input("enter the multipel of 5 :"))
+
+# if (number % 5 == 0):
+#     print("Yes,",number," this is a multiple of 5")
+# else:
+#     print("NO,",number,"is not a multiple of 5 ")
+
+
+
+
+
+# n = int(input("Enter the number :"))
+
+# if(n % 2 == 0):
+#     print("Yes",n,"is Even")
+# else:
+#     print("Yes",n," is Odd")
+
+
+
