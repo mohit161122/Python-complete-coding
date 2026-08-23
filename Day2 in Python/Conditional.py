2117f9aff4515ba702aeb8f650e6d3f5d868cb2e
Mohit <mohitbaghel1234321@gmail.com>
2026-08-23 09:38:22 +0530

    vdfw

---
+++ Day2 in Python/Conditional.py
@@ -0,0 +1,15 @@
+
+# x = 10
+# if (x > 5):
+#     x += 1
+#     print(x)
+
+
+# age = int(input("enter your age :"))
+
+# if (age >= 18):
+#     print("you can vote \n you can drive ")
+# else : 
+#     print("you can't vote ")
+   
+
