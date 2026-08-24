8a7d25dd62027d1e11d506f198d8b6d0e89f3bae
Mohit <mohitbaghel1234321@gmail.com>
2026-08-24 07:46:14 +0530

    sd

---
+++ Day2 in Python/Loops.py
@@ -0,0 +1,36 @@
+#infinite loop
+
+# print 1 to 10
+# i = 1
+
+# while i <= 10: 
+#     print("Hello world" , i)
+#     i +=1
+# print("after loop, count = ", i)
+
+
+
+#reverse 1 to 5
+# i = 5
+
+# while i >= 1:
+#     print(i)
+#     i-=1
+
+
+
+# print 0 to 4
+# i = 0
+# while ( i < 5):
+#     print(i)
+#     i+=1
+
+
+# Multiplication table of any number
+# table = int(input("Enter the number :"))
+# i = 1
+
+# while ( i <= 10):
+#     print(table,"X",i,"=",table * i )
+#     i+=1
+  
