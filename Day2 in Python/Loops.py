ee9dff604e87bcb2fe5acfa1bf0f256d626a0868
Mohit <mohitbaghel1234321@gmail.com>
2026-08-24 08:27:02 +0530

    ceda

--- Day2 in Python/Loops.py
+++ Day2 in Python/Loops.py
@@ -34,3 +34,37 @@
 #     print(table,"X",i,"=",table * i )
 #     i+=1
   
+
+
+
+#use break Keyword 
+# i = 1
+# while( i <= 10):
+#     if(i % 6 == 0):
+#         break
+#     print(i)
+#     i+=1  # update
+
+# print("Outside Loop Now .......")
+
+
+#use continue Keyword to ship only 3
+
+# i = 1
+# while( i <= 5):
+#     if(i % 3 == 0):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+# print("Outside Loop now .....")
+
+
+i = 0
+
+while( i < 10):
+    i+=1
+    if(i % 2 == 0):
+        continue
+    print(i)
