78598c9aa912d6bb30ed65fb6a68c3d0c4261050
Mohit <mohitbaghel1234321@gmail.com>
2026-08-25 14:20:16 +0530

    veh

--- Day2 in Python/Loops.py
+++ Day2 in Python/Loops.py
@@ -1,64 +1,66 @@
-#infinite loop
+# infinite loop
 
-# print 1 to 10
-# i = 1
+print 1 to 10
+i = 1
 
-# while i <= 10: 
-#     print("Hello world" , i)
-#     i +=1
-# print("after loop, count = ", i)
+while i <= 10: 
+    print("Hello world" , i)
+    i +=1
+print("after loop, count = ", i)
 
 
 
 #reverse 1 to 5
-# i = 5
+i = 5
 
-# while i >= 1:
-#     print(i)
-#     i-=1
-
-
-
-# print 0 to 4
-# i = 0
-# while ( i < 5):
-#     print(i)
-#     i+=1
+while i >= 1:
+    print(i)
+    i-=1
 
 
-# Multiplication table of any number
-# table = int(input("Enter the number :"))
-# i = 1
 
-# while ( i <= 10):
-#     print(table,"X",i,"=",table * i )
-#     i+=1
+print 0 to 4
+i = 0
+while ( i < 5):
+    print(i)
+    i+=1
+
+
+  #  Multiplication table of any number
+table = int(input("Enter the number :"))
+i = 1
+
+while ( i <= 10):
+    print(table,"X",i,"=",table * i )
+    i+=1
   
 
 
 
-#use break Keyword 
-# i = 1
-# while( i <= 10):
-#     if(i % 6 == 0):
-#         break
-#     print(i)
-#     i+=1  # update
+ # use break Keyword 
+i = 1
+while( i <= 10):
+    if(i % 6 == 0):
+        break
+    print(i)
+    i+=1  # update
 
-# print("Outside Loop Now .......")
+print("Outside Loop Now .......")
 
 
-#use continue Keyword to ship only 3
+ # use continue Keyword to ship only 3
 
-# i = 1
-# while( i <= 5):
-#     if(i % 3 == 0):
-#         i+=1
-#         continue
-#     print(i)
-#     i+=1
+i = 1
+while( i <= 5):
+    if(i % 3 == 0):
+        i+=1
+        continue
+    print(i)
+    i+=1
 
-# print("Outside Loop now .....")
+print("Outside Loop now .....")
+
+
 
 
 i = 0
@@ -68,3 +70,48 @@ while( i < 10):
     if(i % 2 == 0):
         continue
     print(i)
+
+
+
+# for Loops
+
+string = "hello"   #O
+
+if 'e' in string:
+    print("E exists in string")
+
+for var in string:
+    print(var)
+
+
+for i in range(5):
+    print("Hello world")
+
+
+word = "artificial-intelligence"
+
+count = 0
+
+for ch in word:
+    if (ch == 'i'):
+        count +=1
+
+print("count of i =", count)
+    
+
+
+#  vowel
+word = "artificial"
+count = 0
+
+for ch in word:
+    if ((ch == "i") or (ch == "e") or (ch == "a") or (ch == "o") or (ch == "u")):
+        count +=1
+
+print("count of Vowel count is " ,count)
+
+
+
+
+
+
