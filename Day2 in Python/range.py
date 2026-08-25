572a554298ceb37c09643aa7d15181635bbd142b
Mohit <mohitbaghel1234321@gmail.com>
2026-08-25 15:57:54 +0530

    cdas

--- Day2 in Python/range.py
+++ Day2 in Python/range.py
@@ -1,18 +1,19 @@
-##  range
+#  range
 
-# for i in range(2,11,2):
-#     print(i)
+for i in range(2,11,2):
+    print(i)
 
 
 
-## sum of n number 
+# sum of n number 
 
-# n = int (input("enter the number :"))
-# sum = 0
+n = int (input("enter the number :"))
+sum = 0
 
-# for i in range(1,n+1):
-#     sum += i
+for i in range(1,n+1):
+    sum += i
 
-# print(sum)
+print(sum)
 
-def function():
+
+
