a7d7442f78978abd9b1f9394ac94cb3d962d67ec
Mohit <mohitbaghel1234321@gmail.com>
2026-08-27 15:46:21 +0530

    dkj

---
+++ Day3.py/Insert.py
@@ -0,0 +1,9 @@
+# nums = [1,2,3,4,5,6,7,8,9]
+# nums.insert(3,10)
+# print(nums)
+
+
+nums = [1,2,3,4,5,6,7,8,9,]
+
+nums.insert(8,15)
+print(nums)
--- Day3.py/lists.py
+++ Day3.py/lists.py
@@ -1,10 +1,21 @@
-# matrks1 = 99
-# matrks2 = 99
-# matrks3 = 79
-# matrks4 = 69
-# matrks5 = 59
+matrks1 = 99
+matrks2 = 99
+matrks3 = 79
+matrks4 = 69
+matrks5 = 59
 
 # or 
 
 marks = [99,89,100,65,92]
-print(marks[4])
+
+marks[2] = 70
+
+print(marks[2])
+
+marks = [99,89,100,65,92,"abc"]
+print(type(marks))
+
+
+
+marks = [99,89,100,65,92,]
+print(marks[-5:-2])
