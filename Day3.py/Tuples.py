899df43face116d0a08559190479112f2fe332d7
Mohit <mohitbaghel1234321@gmail.com>
2026-08-28 14:38:39 +0530

    rgvew

---
+++ Day3.py/Tuples.py
@@ -0,0 +1,18 @@
+# tup = (1,2,3,4,5)
+
+# print(tup)
+# print(type(tup))
+# print(len(tup))
+# print(tup[2])
+
+# sum = 0
+# for val in tup:
+#     sum += val
+# print(f"sum if vals in {sum}")
+     
+
+# tup = (1,2,2,3,2,2,4)
+# print(tup.index(2))
+# print(tup.count(2))
+
+
--- Day3.py/tempCodeRunnerFile.py
+++ Day3.py/tempCodeRunnerFile.py
@@ -1,5 +1,3 @@
-word = "i Study from apnacollege"
-# print(word[13:24])
-
-# word = "i Study from apnacollege"
-# print(word[13:])
+print(info.keys())
+print(info.values())
+print(info.items())
