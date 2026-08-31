b554574d79395960f6acdcd6661233dd2f30c804
Mohit <mohitbaghel1234321@gmail.com>
2026-08-31 21:43:07 +0530

    ver

--- Day2 in Python/loopa.py
+++ Day2 in Python/loopa.py
@@ -66,8 +66,35 @@
 
 # string = "hello"
 
-# for var in string:
-#     print(var)
+# # for var in string:
+# #     print(var)
+
+
+# if "o" in string:
+#     print("O Exists in Sting")
+
+
+# for i in range(5):
+#     print("Hello")
+
+# word = "artificial intelligence"
+# count = 0
+
+# for ch in word:
+#     if (ch == "i"):
+#         count +=1
+    
+# print("count of i",count)
+
+# for ch in word:
+#     if( (ch == "a") or ( ch == "e") or (ch == "i") or (ch =="o" ) or (ch == "u")):
+#         count +=1
+# print("Voewl is :",count)
 
 
 
+for i in range( 2,12,2):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
