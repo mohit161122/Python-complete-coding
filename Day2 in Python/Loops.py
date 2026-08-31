ab4dcbbe5464e1303fb16cea7e64e1f9424dbb12
Mohit <mohitbaghel1234321@gmail.com>
2026-08-31 11:00:13 +0530

    jbii

--- Day2 in Python/Loops.py
+++ Day2 in Python/Loops.py
@@ -1,64 +1,66 @@
-#infinite loop
+# infinite loop
 
-# print 1 to 10
-# i = 1
+print 1 to 10
+i = 1
 
-# while i <= 10: 
-#     print("Hello world" , i)
-#     i +=1
-# print("after loop, count = ", i)
+while i <= 10: 
+    print("Hello world" , i)
+    i +=1
+print("after loop, count = ", i)
 
 
 
-#reverse 1 to 5
-# i = 5
+# reverse 1 to 5
+i = 5
 
-# while i >= 1:
-#     print(i)
-#     i-=1
+while i >= 1:
+    print(i)
+    i-=1
 
 
 
 # print 0 to 4
-# i = 0
-# while ( i < 5):
-#     print(i)
-#     i+=1
+i = 0
+while ( i < 5):
+    print(i)
+    i+=1
 
 
- # Multiplication table of any number
-# table = int(input("Enter the number :"))
-# i = 1
+#  Multiplication table of any number
+table = int(input("Enter the number :"))
+i = 1
 
-# while ( i <= 10):
-#     print(table,"X",i,"=",table * i )
-#     i+=1
+while ( i <= 10):
+    print(table,"X",i,"=",table * i )
+    i+=1
   
 
 
 
-#use break Keyword 
-# i = 1
-# while( i <= 10):
-#     if(i % 6 == 0):
-#         break
-#     print(i)
-#     i+=1  # update
-
-# print("Outside Loop Now .......")
+# use break Keyword 
 
 
-#use continue Keyword to ship only 3
+i = 1
+while( i <= 10):
+    if(i % 6 == 0):
+        break
+    print(i)
+    i+=1  # update
 
-# i = 1
-# while( i <= 5):
-#     if(i % 3 == 0):
-#         i+=1
-#         continue
-#     print(i)
-#     i+=1
+print("Outside Loop Now .......")
 
-# print("Outside Loop now .....")
+
+# use continue Keyword to ship only 3
+
+i = 1
+while( i <= 5):
+    if(i % 3 == 0):
+        i+=1
+        continue
+    print(i)
+    i+=1
+
+print("Outside Loop now .....")
 
 
 i = 0
