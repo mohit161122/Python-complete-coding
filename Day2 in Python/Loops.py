0ee52fdb28334fa2eb433953722da54e39274636
Mohit <mohitbaghel1234321@gmail.com>
2026-08-31 12:17:35 +0530

    csdkj

--- Day2 in Python/Loops.py
+++ Day2 in Python/Loops.py
@@ -1,6 +1,6 @@
 # infinite loop
 
-print 1 to 10
+ # print 1 to 10
 i = 1
 
 while i <= 10: 
@@ -10,103 +10,103 @@ print("after loop, count = ", i)
 
 
 
-#reverse 1 to 5
-# i = 5
+# #reverse 1 to 5
+# # i = 5
 
-while i >= 1:
-    print(i)
-    i-=1
-
-
-
-# print 0 to 4
-# i = 0
-# while ( i < 5):
+# while i >= 1:
 #     print(i)
+#     i-=1
+
+
+
+# # print 0 to 4
+# # i = 0
+# # while ( i < 5):
+# #     print(i)
+# #     i+=1
+
+
+#  # Multiplication table of any number
+# # table = int(input("Enter the number :"))
+# # i = 1
+
+# while ( i <= 10):
+#     print(table,"X",i,"=",table * i )
 #     i+=1
-
-
- # Multiplication table of any number
-# table = int(input("Enter the number :"))
-# i = 1
-
-while ( i <= 10):
-    print(table,"X",i,"=",table * i )
-    i+=1
   
 
 
 
-#use break Keyword 
+# #use break Keyword 
+# # i = 1
+# # while( i <= 10):
+# #     if(i % 6 == 0):
+# #         break
+# #     print(i)
+# #     i+=1  # update
+
+# print("Outside Loop Now .......")
+
+
+# #use continue Keyword to ship only 3
+
 # i = 1
-# while( i <= 10):
-#     if(i % 6 == 0):
-#         break
+# while( i <= 5):
+#     if(i % 3 == 0):
+#         i+=1
+#         continue
 #     print(i)
-#     i+=1  # update
+#     i+=1
 
-print("Outside Loop Now .......")
-
-
-#use continue Keyword to ship only 3
-
-i = 1
-while( i <= 5):
-    if(i % 3 == 0):
-        i+=1
-        continue
-    print(i)
-    i+=1
-
-# print("Outside Loop now .....")
+# # print("Outside Loop now .....")
 
 
-i = 0
+# i = 0
 
-while( i < 10):
-    i+=1
-    if(i % 2 == 0):
-        continue
-    print(i)
+# while( i < 10):
+#     i+=1
+#     if(i % 2 == 0):
+#         continue
+#     print(i)
 
 
 
-# for Loops
+# # for Loops
 
-string = "hello"   #O
+# string = "hello"   #O
 
-if 'e' in string:
-    print("E exists in string")
+# if 'e' in string:
+#     print("E exists in string")
 
-for var in string:
-    print(var)
-
-
-for i in range(5):
-    print("Hello world")
+# for var in string:
+#     print(var)
 
 
-word = "artificial-intelligence"
+# for i in range(5):
+#     print("Hello world")
 
-count = 0
 
-for ch in word:
-    if (ch == 'i'):
-        count +=1
+# word = "artificial-intelligence"
 
-print("count of i =", count)
+# count = 0
+
+# for ch in word:
+#     if (ch == 'i'):
+#         count +=1
+
+# print("count of i =", count)
     
 
 
-#  vowel
-word = "artificial"
-count = 0
+# #  vowel
+# word = "artificial"
+# count = 0
 
-for ch in word:
-    if ((ch == "i") or (ch == "e") or (ch == "a") or (ch == "o") or (ch == "u")):
-        count +=1
+# for ch in word:
+#     if ((ch == "i") or (ch == "e") or (ch == "a") or (ch == "o") or (ch == "u")):
+#         count +=1
 
-print("count of Vowel count is " ,count)
+# print("count of Vowel count is " ,count)
 
 
 
---
+++ Day2 in Python/loopa.py
@@ -0,0 +1,64 @@
+# x = int(input("Enter your age: "))
+# if(x >= 18):
+#     print("You are eligible for voting")
+# else:
+#     print("You are not eligible for voting")
+
+
+# color = input("Enter your color: ")
+
+# if (color == "Red"):
+#     print("Stop")
+# elif (color == "Yellow"):
+#         print("Get Ready")
+# elif (color == "Green"):
+#       print("Go")
+# else:
+#       print("Invalid color")
+
+# age = int(input("Enter your age: "))
+
+# if (age < 13): # > 13
+#     print("You are a child")
+# elif((age >= 13) and (age <=18) ): # age between 13 to 18
+#     print("You are a teenager")
+# else:  # age 18+
+#     print("You are an adult")
+
+
+# username = input("Enter your username: ")
+# password = input("Enter your password: ")
+
+# if (username == "admin" and password == "pass"):
+#     print("Login Successful")
+# elif( username != "admin"):
+#     print("wrong username")
+# else:
+#     print("wrong password")
+
+
+# i = int(input("Enter your number: "))
+
+# if (i % 5 == 0 ):
+#     print("Number is divisible by 5")
+# else:
+#     (print("Number is not divisible by 5"))
+
+
+# i = int(input("Enter your number: "))
+
+# if (i % 2 ==0):
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if (username == "admin" and password == "pass"):
+    print("Login Successful")
+else:
+    if( username != "admin"):
+        print("wrong username")
+    else:
+        print("wrong password")
