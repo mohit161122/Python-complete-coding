5e1464f1ca60b1b6c04c406e54a3c91a57a1ab7e
Mohit <mohitbaghel1234321@gmail.com>
2026-09-11 16:46:33 +0530

    fdjkah

--- Day3.py/F-String.py
+++ Day3.py/F-String.py
@@ -1,30 +1,56 @@
-word1 = "Python"
-word2 = "I love "
-wrod3 = "I stufy from Apnacollege"
+# # word1 = "Python"
+# # word2 = "I love "
+# # wrod3 = "I stufy from Apnacollege"
 
-# # print(len(word))
-# # print(word[1])
-# # print(word2 + word1)
-# # print(word1[0])
-# # print(word1[1])
-# # print(word1[2])
-# # print(word1[3])
-# # print(word1[4])
-# # # print(word1[5])
+# # # # print(len(word))
+# # # # print(word[1])
+# # # # print(word2 + word1)
+# # # # print(word1[0])
+# # # # print(word1[1])
+# # # # print(word1[2])
+# # # # print(word1[3])
+# # # # print(word1[4])
+# # # # # print(word1[5])
 
-# # for ch in word1:
-# #     print(ch)
+# # # # for ch in word1:
+# # # #     print(ch)
 
-# print(word2[2])
+# # # print(word2[2])
 
-# print(wrod3[13:len(wrod3)])
+# # # print(wrod3[13:len(wrod3)])
 
-# print(word1[-4:-2])
+# # # print(word1[-4:-2])
 
-a = 5
-b = 8
-# sum = a + b
+# # a = 10
+# # b = 5
+# # # sum = a + b
 
-# print("Sum is {}", format(sum))
-print(f"sum of {a} and {b} is {a+b}")
+# # # print("Sum is {}", format(sum))
+# # print(f"sum of {a} and {b} is {(a+b)/2}")
 
+
+# marks = [88,89,90,92,55,96,98,99,100]
+# # print(marks[5])
+# # print(len(marks))
+# # marks[2] = 70
+# # print(marks)
+# # print(marks[-5:-2])
+
+# # marks.append(5)
+# # print(marks)
+
+
+# # marks.insert(3,50)
+# # print(marks)
+
+# # marks.sort()
+# # print(marks)
+
+# marks.reverse()
+# print(marks)
+
+
+nums = [1,2,3,10,4]
+
+for val in nums:
+    print(val)
