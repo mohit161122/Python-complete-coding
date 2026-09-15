9d7a6bd9c8f2f4bdcea2c6bd7ab43f2341d9f5dd
Mohit <mohitbaghel1234321@gmail.com>
2026-09-15 18:08:38 +0530

    vewrnb

--- Day 5 in Python/datajson.py
+++ Day 5 in Python/datajson.py
@@ -1,8 +1,44 @@
+
+# # json convert into PY_STR
+# import json
+
+
+# json_str = '{"name": "mohit" , "isTeacher": null}'
+
+# py_obj = json.loads(json_str)
+
+# print(type(py_obj) , py_obj)
+
+
+
+
+## PY_obj convert into JSON
+
+# import json
+
+# py_Obj = {
+#     "name":"mohit",
+#     "isTeacher": True
+# }
+
+# json_str = json.dumps(py_Obj)
+# print(type(json_str) , json_str)
+
+
+# import json
+
+# with open("data.json", "r") as f:
+#     py_obj = json.load(f)
+#     print( type(py_obj)  ,py_obj)
+
+
 import json
 
+data = {
+    "name": "mohit",
+    "age": 27,
+    "isTeacher": True
+}
 
-json_str = '{"name":"Alice", "age": 25,}'   
-
-py_obj = json.loads(json_str)
-
-print(type(json_str))
+with open("data.json", "w") as f:
+    json.dump(data, f, indent=4,sort_keys=True)`1`
